f297cffb118951b6a2aed7351109dbf34f3ce7ae
Shannon Stephenson <stephenson.shannon29@gmail.com>
2022-04-06 22:37:02 -0400

    added gluten check to show page

--- views/show.jsx
+++ views/show.jsx
@@ -9,6 +9,12 @@ function Show({ bread, index }) {
       <h2>Show Page</h2>
       <h3>{bread.name}</h3>
       <img src={bread.image} alt={bread.name} />
+      <p>
+        {bread.name}
+        {bread.hasGluten ? <span> does </span> : <span> does NOT </span>}
+        have gluten.
+      </p>
+
       <p>{bread.getBakedBy()}</p>
       <a href={`/breads/${bread.id}/edit`}>
         <button>Edit</button>
@@ -16,6 +22,11 @@ function Show({ bread, index }) {
       <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
         <input type="submit" value="DELETE" />
       </form>
+      <div className="backButton">
+        <a href="/breads">
+          <button>Go back to the index</button>
+        </a>
+      </div>
     </Default>
   )
 }
